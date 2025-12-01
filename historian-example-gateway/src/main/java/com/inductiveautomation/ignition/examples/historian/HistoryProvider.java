8882d7b512ba1e5fb90e85203b637f251329f2b4
kylechase <thekylechase@gmail.com>
2025-12-01 12:59:24 -0700

    initial commit

---
+++ historian-example-gateway/src/main/java/com/inductiveautomation/ignition/examples/historian/HistoryProvider.java
@@ -0,0 +1,115 @@
+package com.inductiveautomation.ignition.examples.historian;
+
+import com.inductiveautomation.historian.common.model.AggregationType;
+import com.inductiveautomation.historian.common.model.data.AtomicPoint;
+import com.inductiveautomation.historian.common.model.data.ChangePoint;
+import com.inductiveautomation.historian.common.model.data.ComplexPoint;
+import com.inductiveautomation.historian.common.model.data.StorageResult;
+import com.inductiveautomation.historian.common.model.options.AggregatedQueryOptions;
+import com.inductiveautomation.historian.common.model.options.ComplexQueryKey;
+import com.inductiveautomation.historian.common.model.options.ComplexQueryOptions;
+import com.inductiveautomation.historian.common.model.options.RawQueryOptions;
+import com.inductiveautomation.historian.gateway.api.AbstractHistorian;
+import com.inductiveautomation.historian.gateway.api.config.HistorianSettings;
+import com.inductiveautomation.historian.gateway.api.query.QueryEngine;
+import com.inductiveautomation.historian.gateway.api.query.browsing.BrowsePublisher;
+import com.inductiveautomation.historian.gateway.api.query.processor.AggregatedPointProcessor;
+import com.inductiveautomation.historian.gateway.api.query.processor.ComplexPointProcessor;
+import com.inductiveautomation.historian.gateway.api.query.processor.RawPointProcessor;
+import com.inductiveautomation.historian.gateway.api.storage.StorageEngine;
+import com.inductiveautomation.ignition.common.QualifiedPath;
+import com.inductiveautomation.ignition.common.browsing.BrowseFilter;
+import com.inductiveautomation.ignition.gateway.model.GatewayContext;
+import com.inductiveautomation.ignition.gateway.model.ProfileStatus;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CompletionStage;
+
+public class HistoryProvider extends AbstractHistorian<Settings> {
+    GatewayContext context;
+    HistorianSettings settings;
+
+    public HistoryProvider(GatewayContext gatewayContext, String name, Settings settings) {
+        super(gatewayContext, name);
+        this.context = gatewayContext;
+        this.settings = settings;
+    }
+
+    @Override
+    public ProfileStatus getStatus() {
+        return ProfileStatus.RUNNING;
+    }
+
+    @Override
+    public Optional<QueryEngine> getQueryEngine() {
+        return super.getQueryEngine();
+    }
+
+    @Override
+    public Optional<StorageEngine> getStorageEngine() {
+        return super.getStorageEngine();
+    }
+
+    @Override
+    public boolean handleNameChange(String newName) {
+        return super.handleNameChange(newName);
+    }
+
+    @Override
+    public boolean handleSettingsChange(Settings newSettings) {
+        return super.handleSettingsChange(newSettings);
+    }
+
+    @Override
+    public Settings getSettings() {
+        return super.getSettings();
+    }
+
+    class ExampleQueryEngine implements QueryEngine {
+
+        @Override
+        public void browse(QualifiedPath qualifiedPath, BrowseFilter browseFilter, BrowsePublisher browsePublisher) {
+
+        }
+
+        @Override
+        public void query(RawQueryOptions rawQueryOptions, RawPointProcessor rawPointProcessor) {
+
+        }
+
+        @Override
+        public void query(AggregatedQueryOptions options, AggregatedPointProcessor processor) {
+            QueryEngine.super.query(options, processor);
+        }
+
+        @Override
+        public <P extends ComplexPoint<?>, K extends ComplexQueryKey<?>> void query(ComplexQueryOptions<K> options, ComplexPointProcessor<P, K> processor) {
+            QueryEngine.super.query(options, processor);
+        }
+
+        @Override
+        public Collection<? extends AggregationType> getNativeAggregates() {
+            return QueryEngine.super.getNativeAggregates();
+        }
+    }
+
+    class ExampleStorageEngine implements StorageEngine {
+
+        @Override
+        public CompletionStage<StorageResult<AtomicPoint<?>>> storeAtomic(List<AtomicPoint<?>> list) {
+            return null;
+        }
+
+        @Override
+        public <C extends ComplexPoint<?>> CompletionStage<StorageResult<C>> storeComplex(List<C> list) {
+            return null;
+        }
+
+        @Override
+        public <C extends ChangePoint<?>> CompletionStage<StorageResult<C>> applyChanges(List<C> list) {
+            return null;
+        }
+    }
+}
